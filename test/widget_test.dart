1826cc5e21d9fc46480cb337cf23c19e6c8645cd
Nirgranth Mehta <nnmehta1804@gmail.com>
2025-08-08 19:49:39 +0530

    modify

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:swaap/main.dart';
+import 'package:swaap/utils/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
