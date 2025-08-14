5e92c0814d83f4deb902351be5626ffc4b59c36e
Nirgranth Mehta <nnmehta1804@gmail.com>
2025-08-14 13:22:44 +0530

    Build Gradle Commit

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,20 +1,21 @@
-android {
-    namespace = "com.example.app"
-    compileSdk = 34
-
-    defaultConfig {
-        applicationId = "com.example.app"
-        minSdk = 21
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
     }
+}
 
-    ndkVersion = "27.0.12077973"  // ✅ This is the added line
+val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
+rootProject.layout.buildDirectory.value(newBuildDir)
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-        }
-    }
+subprojects {
+    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.value(newSubprojectBuildDir)
+}
+subprojects {
+    project.evaluationDependsOn(":app")
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
 }
